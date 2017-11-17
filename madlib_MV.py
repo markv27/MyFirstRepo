eb2a6003978db5ec2e883ab031049069eeae4c9e
markv27 <33756627+markv27@users.noreply.github.com>
2017-11-17 10:31:01 -0500

    Add files via upload

--- madlib_MV.py
+++ madlib_MV.py
@@ -1,3 +1,4 @@
+import time
 ### Mad Libs ###
 ### Source: http://ordinaryhumanlanguage.ca/blog/wp-content/uploads/2017/07/Mad-Libs-Example.jpg
 
@@ -48,7 +49,7 @@ female1 = input()
 
 print("Give me a noun")
 noun7 = input()
-
+
 print("Give me a plural body part")
 bodypart2 = input()
 
@@ -59,5 +60,12 @@ bodypart2 = input()
 print("It was Thanksgiving and the succlent roast " + noun1 + " wafted through my house.")
 print("'" + person1 + ", it's time to " + verb1 + "!' my mother called.")
 print("I couldn't wait to get my " + bodypart1 + " on that " + adj1 + " Thanksgiving meal.")
+print("My family sat around the dining-room " + noun2 + ".")
+print("The table was laid out with every kind of " + noun3 + " imaginable.")
+print("There was a basket of hot buttered " + pluralnoun1 + " and glass of sparkling " + liquid1 + ".")
+print("The " + adj2 + " turkey sat, steaming, next to a tureen of " + noun4 + " gravy.")
+print("A bowl of ruby-red " + noun5 + " sauce, a sweet- " + noun6 + " casserole, and a dish of " + pluralnoun2 + " tempted by taste buds.")
+print("But the dish I looked forward to most was Grandma " + female1 + "'s famous " + noun7 + " pie.")
+print("Thanksgiving is my favorite holiday, " + bodypart2 + " down.")
 
-
+time.sleep(100)
