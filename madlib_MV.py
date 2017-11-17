6b1c1070a7b54d8beb6233de824c7569710cbd8a
markv27 <33756627+markv27@users.noreply.github.com>
2017-11-17 10:12:21 -0500

    First Upload

---
+++ madlib_MV.py
@@ -0,0 +1,63 @@
+### Mad Libs ###
+### Source: http://ordinaryhumanlanguage.ca/blog/wp-content/uploads/2017/07/Mad-Libs-Example.jpg
+
+print("Give me a noun")
+noun1 = input()
+
+print("Give me a person in room")
+person1 = input()
+
+print("Give me a verb")
+verb1 = input()
+
+print("Give me a plural body part")
+bodypart1 = input()
+
+print("Give me an adjective")
+adj1 = input()
+
+print("Give me a noun")
+noun2 = input()
+
+print("Give me a noun")
+noun3 = input()
+
+print("Give me a plural noun")
+pluralnoun1 = input()
+
+print("Give me a type of liquid")
+liquid1 = input()
+
+print("Give me an adjective")
+adj2 = input()
+
+print("Give me a noun")
+noun4 = input()
+
+print("Give me a noun")
+noun5 = input()
+
+print("Give me a noun")
+noun6 = input()
+
+print("Give me a plural noun")
+pluralnoun2 = input()
+
+print("Give me a female in the room")
+female1 = input()
+
+print("Give me a noun")
+noun7 = input()
+
+print("Give me a plural body part")
+bodypart2 = input()
+
+
+
+### Begin Mad Lib Here ###
+
+print("It was Thanksgiving and the succlent roast " + noun1 + " wafted through my house.")
+print("'" + person1 + ", it's time to " + verb1 + "!' my mother called.")
+print("I couldn't wait to get my " + bodypart1 + " on that " + adj1 + " Thanksgiving meal.")
+
+
---
+++ personality_MV.py
@@ -0,0 +1,18 @@
+name = "Mark"
+state = "Utah"
+city = "San Francisco"
+game = "2K"
+book = "Moneyball"
+
+print(name + " likes to visit " + state + " and " + city)
+print(name + "'s favorite game is " + game + " and reads " + book)
+
+
+print("What's your favorite subject?")
+subject = input()
+
+if subject == "Math":
+    print("That's crazy, me too!")
+else:
+    print(subject + " is alright I guess.")
+    
